03bb461c9b42dd8fade447e797d3287828bd1abf
kostiantyn13 <kos.kondratenko@gmail.com>
2018-01-25 19:03:22 +0200

    1.6

---
+++ src/iteratable.js
@@ -0,0 +1,139 @@
+/*Итератор - это паттерн проэктирования согласно которому источник элементов
+"прячется" от клиента. Клиенту достается специальный обьект, элементы которого уже может
+использовать автор*/
+
+//iteratable (итерируемый, перебираемый обьект) - содержание
+// которого можно перебрать по одному элементу
+//примеры итерируемых обьектов---
+//[4,5,6]
+//'String jjj'
+//[div, div, div] - если это не массив, а набор
+
+//у итерируемого обьекта есть специальный метод, который возвращает обьект(см ниже)
+/*Iterable {
+    [Symbol.iterator]() - -----Iterator - обьект возвращемый данным методом
+}*/
+/*
+let iterator = {
+  [Symbol.iterator]() {
+    return {
+      next() {
+        //всего 1 метод, возвращает обьект - результат итератора
+        return {
+          value: "", //значение след элемента в последовательности!!!!ВНИМАНИЕ - не данного, а следующего
+          done: true //если нет дальнейших свойств для перебора - true, обычно - false
+        };
+      }
+    };
+  }
+};
+*/
+let xman = ["Syclops", "Wolverine", "Rogue"];
+/*
+console.info("for");
+for (let index = 0; index < xman.length; index++) {
+  console.log(xman[index]);
+}
+
+console.info("for...in");
+for (let index in xman) {
+  console.log(xman[index]);
+}
+console.info("forEach");
+xman.forEach(x => console.log(x));
+*/
+console.info("for...of");
+for (let index of xman) {
+  console.log(index);
+}
+
+let iterator = xman[Symbol.iterator]();
+
+//console.log(iterator.next()); //value = Syclops; done = false
+//console.log(iterator.next()); //value = Wolverine; done = false
+//console.log(iterator.next()); //value = Rogue; done = false
+//console.log(iterator.next()); //value = underfined; done = true
+//iterator.length не существует
+
+let next = iterator.next();
+while (!next.done) {
+  console.log(next.value);
+  next = iterator.next();
+}
+
+let idGenerator = {
+  [Symbol.iterator]() {
+    let id = 1;
+    return {
+      next() {
+        let value = id > 100 ? undefined : id++;
+        let done = !value;
+        return { value, done }; //сократили так как value: value
+      }
+    };
+  }
+};
+
+for (let id of idGenerator) {
+  console.log(id);
+}
+//генератор случайных чисел
+let randomGenerator = {
+  generate() {
+    return this[Symbol.iterator]();
+  },
+  [Symbol.iterator]() {
+    let count = 0;
+    return {
+      next() {
+        count++;
+        let value = Math.ceil(Math.random() * 100);
+        let done = count > 10;
+
+        return { value, done }; //сократили так как value: value
+      }
+    };
+  }
+};
+/*
+for (let random of randomGenerator) {
+  console.log(random);
+}
+*/
+console.log(randomGenerator.generate().next().value);
+
+class ArrayIterator {
+  constructor(array) {
+    this.array = array.map(item => item).sort(); //отсортировать массив, но не исходный
+    this.index = 0;
+  }
+  next() {
+    let result = { value: undefined, done: true };
+
+    if (this.index < this.array.length) {
+      result.value = this.array[this.index];
+      result.done = false;
+      this.index++;
+    }
+    return result;
+  }
+}
+
+class Tasks {
+  constructor() {
+    this.tasks = [];
+  }
+  addTasks(...tasks) {
+    this.tasks = this.tasks.concat(tasks);
+  }
+  [Symbol.iterator]() {
+    return new ArrayIterator(this.tasks);
+  }
+}
+
+let taskLit = new Tasks();
+
+taskLit.addTasks("Освоить ES999", "Освоить ES5", "Освоить React");
+for (let task of taskLit) {
+  console.log(task);
+}
--- src/promises.1.js
+++ src/promises.1.js
@@ -8,30 +8,58 @@ function addMovieToList(movie) {
   movieList.appendChild(img);
 }
 
-function getDate(url, done) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      let json = JSON.parse(xhr.response);
-      console.log(json);
-      done(json.Search);
-    } else {
-      console.error(xhr.statusText);
-    }
-  };
-  xhr.onerror = function(error) {
-    console.error(error);
-  };
-  xhr.send();
+function getDate(url) {
+  return new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        let json = JSON.parse(xhr.response);
+        resolve(json.Search);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = function(error) {
+      reject(error);
+    };
+    xhr.send();
+  });
 }
 
-let search = "007";
-getDate(
-  `http://www.omdbapi.com/?i=tt3896198&apikey=9450f321&s=${search}`,
-  function(movies) {
-    movies.forEach(function(movie) {
-      addMovieToList(movie);
-    }, this);
-  }
+let batman = getDate(
+  "http://www.omdbapi.com/?i=tt3896198&apikey=9450f321&s=batman"
 );
+let superman = getDate(
+  "http://www.omdbapi.com/?i=tt3896198&apikey=9450f321&s=superman"
+);
+/*
+//какая из функций поиска выполнится первой в таком порядке и будут выведены фото batman and superman
+batman
+  .then(movies => movies.forEach(movie => addMovieToList(movie)))
+  .catch(error => console.error(error));
+superman
+  .then(movies => movies.forEach(movie => addMovieToList(movie)))
+  .catch(error => console.error(error));*/
+Promise.race([batman, superman])
+  .then(movies => movies.forEach(movie => addMovieToList(movie)))
+  .catch(error => console.error(error));
+/*
+function go(num) {
+  return new Promise(function(resolve, reject) {
+    let delay = Math.ceil(Math.random() * 3000);
+    console.log(num, delay);
+    setTimeout(() => {
+      if (delay > 2000) {
+        reject(num);
+      } else resolve(num);
+    }, delay);
+  });
+}
+
+let [p1, p2, p3] = [go(1), go(2), go(3)];
+//Promise.race([p2, p1, p3]); //какое первое выполнится, с аргументом того и работаем, нет так нет
+Promise.all([p2, p1, p3]) //должны выполнится все условия(работаем с аргументом последнего)
+  .then(value => console.log(value))
+  .catch(error => console.error(error));
+*/
